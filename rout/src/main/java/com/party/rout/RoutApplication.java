ec5f381f75a78b0476bc615844f42301430f4efb
Валерий Черноусов <vchernousov@logrocon.net>
2020-07-11 20:00:46 +0300

    git init

---
+++ rout/src/main/java/com/party/rout/RoutApplication.java
@@ -0,0 +1,13 @@
+package com.party.rout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutApplication.class, args);
+	}
+
+}
