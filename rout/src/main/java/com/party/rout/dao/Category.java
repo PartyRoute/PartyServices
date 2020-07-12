ac9d5d95cddf0f0004b0ca6ed27b3895875d5c31
Валерий Черноусов <vchernousov@logrocon.net>
2020-07-13 02:22:53 +0300

    add class Category

---
+++ rout/src/main/java/com/party/rout/dao/Category.java
@@ -0,0 +1,56 @@
+package com.party.rout.dao;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column(name = "parent_id")
+    private int parentId;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(int parentId) {
+        this.parentId = parentId;
+    }
+
+
+}
